7ad9f28a34f5d7194073fbc59d7fe8ee840c77f4
owais parkar <owaisparkar94@outlook.com>
2020-07-16 12:29:45 +1200

    Updates

---
+++ precache-manifest.59b11bbfbd821d51aeccc1cc4bd1ccbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53446ffbf9ab3a0ce3f744f57ac96d95",
+    "url": "/myTodo/index.html"
+  },
+  {
+    "revision": "740a0f76244e4bb2c4bb",
+    "url": "/myTodo/static/css/main.73dd90ff.chunk.css"
+  },
+  {
+    "revision": "6276fd4e820780482b9a",
+    "url": "/myTodo/static/js/2.c4e2a6b9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myTodo/static/js/2.c4e2a6b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740a0f76244e4bb2c4bb",
+    "url": "/myTodo/static/js/main.c71797da.chunk.js"
+  },
+  {
+    "revision": "04ed8cb6a3ff8ca2b8b8",
+    "url": "/myTodo/static/js/runtime-main.f3248e00.js"
+  },
+  {
+    "revision": "3187ad17774ac7e26f4032d24db63436",
+    "url": "/myTodo/static/media/ToDoList.3187ad17.jpg"
+  }
+]);
